e330aaa440aa5741a534e80b998a5477648e275b
Jeffrey Matos <matos.jeffrey.j@gmail.com>
2019-07-22 16:36:41 -0400

    Development Mode Error

--- SevensPizzaAPI/Startup.cs
+++ SevensPizzaAPI/Startup.cs
@@ -48,14 +48,16 @@ namespace SevensPizzaAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseHsts();
+            //}
 
             app.UseCors("CorsPolicy");
 
